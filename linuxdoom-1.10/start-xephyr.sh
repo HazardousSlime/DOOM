36360fde8d39e1a2154ba1d0fcce3e775dce57b6
Richard Reynolds <xacalite@yahoo.com>
2022-12-13 14:20:20 -0800

    No longer complains about sounds not being pre-cached

--- linuxdoom-1.10/start-xephyr.sh
+++ linuxdoom-1.10/start-xephyr.sh
@@ -1,4 +1,3 @@
 DISPLAY=:0
-Xephyr :1 -ac -screen 960x600x8 & DISPLAY=:1
-DISPLAY=:1
+Xephyr :1 -ac -screen 960x600x8 -reset -terminate -title DOOM & DISPLAY=:1
 #Xephyr :1 -cc 2 -screen 960x600x8 & DISPLAY=:1
